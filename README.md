09ebfa6fec6c65c1cb9aef4cb11e2e261a9c76d0
Mathias Stadler <stadler-mathias@web.de>
2024-07-08 10:06:52 +0200

    save

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # xvfb XTerminal on debian base
+
+## save
+
+docker run -it --mount type=bind,src="$(pwd)",target=/src ubuntu bash
+
+## development container
+
+https://docs.docker.com/guides/workshop/06_bind_mounts/
+
+docker run -it --userns host --privileged --security-opt=seccomp=unconfined ubuntu bash
+
+docker run -it --userns host --privileged --security-opt=seccomp=unconfined --mount type=bind,src="$(pwd)",target=/src ubuntu bash 
--- start_container_raspi_debian_xvfb.sh
+++ start_container_raspi_debian_xvfb.sh
@@ -45,6 +45,36 @@ docker run -it \
 -p 5959:5900 \
 debian_stage_s8
 
+## /w --host
+docker run -it \
+--security-opt apparmor=unconfined \
+--cap-add SYS_ADMIN \
+--cap-add CAP_SYSLOG \
+--cap-add CAP_PERFMON \
+--cap-add CAP_BPF \
+--cap-add CAP_CHECKPOINT_RESTORE \
+--cap-add CAP_NET_RAW \
+--cap-add CAP_SYS_CHROOT \
+--cap-add CAP_AUDIT_WRITE \
+--cap-add CAP_MKNOD \
+--tmpfs /run \
+--volume /sys/fs/cgroup:/sys/fs/cgroup \
+--volume /dev/shm:/dev/shm \
+--device /dev/snd \
+--device /dev/dri \
+--volume /sys/fs/cgroup:/sys/fs/cgroup \
+--volume /dev/shm:/dev/shm \
+--volume ./workspace:/home/user/workspace:rw \
+--env PULSE_SERVER=unix:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume "${XDG_RUNTIME_DIR}"/pulse/native:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+--group-add "$(getent group audio | cut -d: -f3)" \
+--hostname debian_stage_s8_cap_complete \
+-p 2222:22 \
+-p 5959:5900 \
+--host \
+debian_stage_s8
+
 ## w/o apparmor and workspace
 docker run -it \
 --tmpfs /run \
