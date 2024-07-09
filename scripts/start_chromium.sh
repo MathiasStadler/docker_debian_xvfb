1dc694d5a66bcbd07cf867029239838c64963673
Mathias Stadler <stadler-mathias@web.de>
2024-07-09 12:09:11 +0200

    add

---
+++ scripts/start_chromium.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -euxo
+# export PULSE_SERVER=unix:/pulse/native
+export PULSE_SERVER=unix:/run/user/1000/pulse/native
+export DISPLAY=:1
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+export XDG_RUNTIME_DIR=/run/user/1000
+export HOME=/home/user
+xhost +
+# google-chrome-stable --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage
+# --disable-gpu --disable-software-rasterizer
+# google-chrome-stable --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --no-init
+/usr/bin/chromium --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --no-init
