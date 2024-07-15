8b2e27b4be0fcf1f50d07a5fb9c887b1adecbfed
Mathias Stadler <stadler-mathias@web.de>
2024-07-15 09:01:29 +0200

    add

---
+++ scripts/install_vscode_extention.sh
@@ -0,0 +1,11 @@
+# via terminal
+
+# list all installed extentions
+code --list-extensions | xargs -L 1 echo code --install-extension
+
+# show all installed extentions
+code --install-extension Extensions installed on SSH: 192.168.178.40:2224:
+code --install-extension fleshywaffles.vs-code-clippy
+code --install-extension rust-lang.rust-analyzer
+code --install-extension serayuzgur.crates
+code --install-extension vadimcn.vscode-lldb
