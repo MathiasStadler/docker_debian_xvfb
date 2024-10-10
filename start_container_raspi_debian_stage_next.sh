7cc3fae5b4dec1eeef403d64fe66c4bf65ac1c20
Mathias Stadler <stadler-mathias@web.de>
2024-10-10 14:02:17 +0200

    save

--- start_container_raspi_debian_stage_next.sh
+++ start_container_raspi_debian_stage_next.sh
@@ -55,3 +55,37 @@ debian_stage_next
 
 # docker inspect <container id>
 # docker inspect 4afd7d8722be
+
+# perl
+# docker volume create workspace_perl_one
+# docker volume ls
+
+docker run  \
+--security-opt apparmor=unconfined \
+--security-opt seccomp=unconfined \
+--cap-add SYS_ADMIN \
+--cap-add CAP_SYSLOG \
+--cap-add CAP_PERFMON \
+--cap-add CAP_BPF \
+--cap-add CAP_CHECKPOINT_RESTORE \
+--cap-add CAP_NET_RAW \
+--cap-add CAP_SYS_CHROOT \
+--cap-add CAP_AUDIT_WRITE \
+--cap-add CAP_MKNOD \
+--cap-add SYS_PTRACE \
+--tmpfs /run \
+--volume /sys/fs/cgroup:/sys/fs/cgroup \
+--volume /dev/shm:/dev/shm \
+--device /dev/snd \
+--device /dev/dri \
+--volume /sys/fs/cgroup:/sys/fs/cgroup \
+--volume /dev/shm:/dev/shm \
+--volume workspace_perl_two:/home/user/workspace_perl:rw \
+--env PULSE_SERVER=unix:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume "${XDG_RUNTIME_DIR}"/pulse/native:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+--group-add "$(getent group audio | cut -d: -f3)" \
+--hostname debian_stock_two \
+-p 2258:22 \
+-p 5958:5900 \
+debian_stage_next
